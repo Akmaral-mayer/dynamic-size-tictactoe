daa55bee25f9382cd610a0fa94c4dce928ce86e1
Islamova Akmaral <akmaralslmv@gmail.com>
2020-02-20 11:20:29 +0600

    Updates

---
+++ precache-manifest.1cab71c1736ed31ca3eb4cfd6c37f1ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4a60e934aadd9356eaf5852f6333e9",
+    "url": "/dynamic-size-tictactoe/index.html"
+  },
+  {
+    "revision": "98e02cfe3c022d2472b1",
+    "url": "/dynamic-size-tictactoe/static/css/main.f7358a91.chunk.css"
+  },
+  {
+    "revision": "c91564fb5bf2cb2cc5d4",
+    "url": "/dynamic-size-tictactoe/static/js/2.c5a97fef.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dynamic-size-tictactoe/static/js/2.c5a97fef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e02cfe3c022d2472b1",
+    "url": "/dynamic-size-tictactoe/static/js/main.9d1f141b.chunk.js"
+  },
+  {
+    "revision": "baae7a5f70673c80147f",
+    "url": "/dynamic-size-tictactoe/static/js/runtime-main.b0973165.js"
+  }
+]);
